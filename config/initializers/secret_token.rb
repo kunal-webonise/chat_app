f25e618f7d8456aad0c0d8841082ed4d8c90b6be
Kunal Bhatia <kunalb@weboniselab.com>
2015-03-11 16:18:55 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChatApp::Application.config.secret_key_base = '8d9228462e53b1816d9ce3af896d38e6489ae87625c94289f202a17d33c8372beef0e3f1642b90d27f15c3d1a21a2379c3fc1abaf005ce44b8b200af2ea084fd'
